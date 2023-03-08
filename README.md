b329aef628407abd30be53e35482aac85c39196c
axuint <kaptenmojo@gmail.com>
2023-03-08 19:24:31 +0700

    IDK bruh what is that

---
+++ README.md
@@ -0,0 +1,29 @@
+# Waifu Speech
+Waifu Speech is a program for changing voices into female anime voices.
+
+### How does Waifu Speech work
+<img src="/assets/images/structure.png" />
+If you look at the diagram there are 4 processes in this program. Because Voicevox only supports Japanese, it needs to change the text of the audio that we previously processed into Japanese
+
+### How do I run this program
+To run this very simple program, you need Python version 3.7++ because this program uses Python, so make sure you have Python installed on your computer.
+
+#### Install modules
+- Windows, Linux, Mac
+``` konsole
+pip install -r requirements.txt
+```
+*  I'm not sure it will work on all platforms but you can give it a try. I tried it on linux
+
+If already installed
+``` konsole
+python3 main.py
+```
+or
+``` konsole
+python main.py
+```
+(I'm using the first method because im using python 3.xx)
+
+### License
+[see: LICENSE](/LICENSE)
---
+++ main.py
@@ -0,0 +1,114 @@
+import translators.server as tss
+import requests
+import speech_recognition as sr
+
+
+class AudioToText:
+    def __init__(self, audio_file):
+        self.audio_file = audio_file
+        self.recognizer = sr.Recognizer()
+
+    def convert(self):
+        try:
+            with sr.AudioFile(self.audio_file) as audio:
+                audio_data = self.recognizer.record(audio)
+            text = self.recognizer.recognize_google(audio_data, language='id-ID')
+            print(f"[Success] You say: {text}")
+            return text
+        except:
+            return None
+            
+class MicToText:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+
+    def convert(self):
+        try:
+            with sr.Microphone() as audio:
+                print("Speak now...")
+                audio_data = self.recognizer.listen(audio)
+            text = self.recognizer.recognize_google(audio_data, language='id-ID')
+            print(f"[Success] You say: {text}")
+            return text
+        except:
+            return None
+
+
+class TextTranslator:
+    def __init__(self, text):
+        self.text = text
+
+    def translate(self, source, target):
+        try:
+            translated_text = tss.google(self.text, source, target)
+            print(f"[Success] Translated: {translated_text}")
+            return translated_text
+        except:
+            return None
+
+
+class TextToSpeech:
+    def __init__(self, text):
+        self.text = text
+
+    def generate_audio(self, speaker_id=1):
+        try:
+            url = f"https://api.tts.quest/v1/voicevox/?text={self.text}&speaker={speaker_id}"
+            response = requests.get(url).json()
+            if response["success"] == True:
+                audio_url = response["mp3DownloadUrl"]
+                return audio_url
+            else:
+                return None
+        except:
+            return None
+
+    def save_audio(self, audio_url, file_name):
+        try:
+            audio_content = requests.get(audio_url).content
+            with open(file_name, "wb") as file:
+                file.write(audio_content)
+            return True
+        except:
+            return False
+
+
+if __name__ == "__main__":
+    audio_file = "testing.wav"
+
+    # convert audio to text
+    audio_to_text = AudioToText(audio_file)
+    text = audio_to_text.convert()
+    if not text:
+        print("[Failed] Failed to convert audio file to text")
+        exit()
+    
+    # Uncomment this if you want to use your microphone as sound source
+    # You need PyAudio first install it use pip install PyAudio
+    # convert microphone to text
+    #audio_to_text = AudioToText(audio_file)
+    #text = audio_to_text.convert()
+    #if not text:
+    #    print("[Failed] Failed to convert audio file to text")
+    #    exit()
+
+    # translate text to Japanese
+    text_translator = TextTranslator(text)
+    translated_text = text_translator.translate("id", "ja")
+    if not translated_text:
+        print("[Failed] Failed to translate text")
+        exit()
+
+    # generate audio file
+    text_to_speech = TextToSpeech(translated_text)
+    audio_url = text_to_speech.generate_audio()
+    if not audio_url:
+        print("[Failed] Failed to generate audio")
+        exit()
+
+    # save audio file
+    file_name = "test.mp3"
+    if text_to_speech.save_audio(audio_url, file_name):
+        print(f"[Success] Audio file saved as {file_name}")
+    else:
+        print("[Failed] Audio file not saved")
